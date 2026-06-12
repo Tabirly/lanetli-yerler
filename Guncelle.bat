f49e83b85f0a65c0974358109ffbca7e42036cb9
Tabirly <metehankuzu1987@gmail.com>
2026-06-12 18:06:23 +0300

    yaptığınız değişikliklerin özeti

---
+++ Guncelle.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Lanetli Yerler sitesi guncelleniyor...
+python update_script.py
+echo.
+echo Guncelleme tamamlandi! Lutfen "Lanetli Yerler.txt" dosyasinin icindeki yeni kodu Blogger'a yapistirin.
+pause
